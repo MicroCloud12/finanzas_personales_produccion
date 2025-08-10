1048f08ce44d984165df0936d60c8e94cbeb2e87
Mauricio Arenas <35950276+MicroCloud12@users.noreply.github.com>
2025-08-09 18:41:14 -0600

    fix: comment out security settings for SSL redirection and secure cookies

--- config/settings.py
+++ config/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Redirige todas las peticiones HTTP a HTTPS.
-SECURE_SSL_REDIRECT = True
+#SECURE_SSL_REDIRECT = True
 
 # Asegura que las cookies de sesión solo se envíen por HTTPS.
-SESSION_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
 
 # Asegura que la cookie CSRF solo se envíe por HTTPS.
-CSRF_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 
 # Application definition
 
