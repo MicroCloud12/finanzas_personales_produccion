fb3f69254aa7ab5e800d2249beb341e9b175a9af
Mauricio Arenas <35950276+MicroCloud12@users.noreply.github.com>
2025-12-11 21:36:50 -0600

    feat: Add admin registrations for financial models and a new header template with user navigation.

--- finanzas/admin.py
+++ finanzas/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import registro_transacciones, TransaccionPendiente, inversiones, Suscripcion
+from .models import registro_transacciones, TransaccionPendiente, inversiones, Suscripcion, PendingInvestment
 
 # Registramos los modelos para que aparezcan en el panel de admin
 admin.site.register(registro_transacciones)
 admin.site.register(TransaccionPendiente)
 admin.site.register(inversiones)
 admin.site.register(Suscripcion) # <-- Esta línea es la importante
+admin.site.register(PendingInvestment)
 #admin.site.register(Venta)
