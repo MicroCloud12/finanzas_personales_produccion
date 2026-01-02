5fc422309a712bbecd407e2a1b574504b900df7c
Mauricio Arenas <35950276+MicroCloud12@users.noreply.github.com>
2026-01-01 21:52:57 -0600

    feat: create initial Django project settings including database, static files, allauth, logging, and session management configurations.

--- config/settings.py
+++ config/settings.py
@@ -171,8 +171,8 @@ LOGOUT_REDIRECT_URL = 'home'
 # config/settings.py
 
 # --- CELERY SETTINGS ---
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'https://redis:6379/0'
+CELERY_RESULT_BACKEND = 'https://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
